8a7ea4061107e40ad8b1f7f0adcb660ac671b2d1
Klamkin, Michael <mklamkin3@gatech.edu>
2025-06-16 20:30:19 -0400

    working kernels

--- exa_mwe.jl
+++ exa_mwe.jl
@@ -14,3 +14,18 @@ exagraph = JuMP.value(
     # *(x,y,p)
 )
 fn = ExaModels._simdfunction(exagraph, 0, 0, 0)  # not sure how offsets work...
+
+using KernelAbstractions, Metal
+
+@kernel function kerfb(y, @Const(f), @Const(itr), @Const(x))
+    I = @index(Global)
+    @inbounds y[ExaModels.offset0(f, itr, I)] = f.f(itr[I], @view(x[:, I]))
+end
+
+y_m = Metal.zeros(1,2)
+itr_m = MtlArray([1.0f0 2.0f0;])
+X_m = MtlArray([2.0f0 5.0f0;3.0f0 8.0f0])
+
+kerfb(MetalBackend())(y_m, fn, itr_m, X_m, ndrange=size(X_m, 2)); y_m
+y_c = fn.f.(eachcol(Matrix(itr_m)), eachcol(Matrix(X_m)))
+@assert all(y_c .== Matrix(y_m)[1, :])
